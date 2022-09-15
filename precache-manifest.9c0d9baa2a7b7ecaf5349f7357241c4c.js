4488c2cb7b10c9362fcacd673622f2536d57fc13
Harsh <harsh@gmail.com>
2022-09-15 19:33:52 +0530

    Updates

---
+++ precache-manifest.9c0d9baa2a7b7ecaf5349f7357241c4c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d9eb9f394ef99054acbe",
+    "url": "/reactapp/static/css/main.5a4cc698.chunk.css"
+  },
+  {
+    "revision": "d9eb9f394ef99054acbe",
+    "url": "/reactapp/static/js/main.d9eb9f39.chunk.js"
+  },
+  {
+    "revision": "97af9aaba510c3e4c76e",
+    "url": "/reactapp/static/js/1.97af9aab.chunk.js"
+  },
+  {
+    "revision": "946d8365c0317c9ba6aa",
+    "url": "/reactapp/static/js/2.946d8365.chunk.js"
+  },
+  {
+    "revision": "79c3b1ba8b1d9cdead6f",
+    "url": "/reactapp/static/js/runtime~main.79c3b1ba.js"
+  },
+  {
+    "revision": "d7dfae4762f5cb21bb31cbd6812efef8",
+    "url": "/reactapp/index.html"
+  }
+];
